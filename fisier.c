100adaec02b47658d10b42ad38b9b83feda209ce
Simina Hategan <siminahategan@gmail.com>
2023-11-18 00:00:59 +0200

    crearea mai multor procese si transformare fisier .bmp in alb-negru

--- fisier.c
+++ fisier.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<stdint.h>
 #include<sys/types.h>
+#include<sys/wait.h>
 #include<sys/stat.h>
 #include<unistd.h>
 #include<errno.h>
@@ -22,6 +23,8 @@ DIR *dir2=NULL;
 int fis;
 char buffer[1024];
 int bytes_written;
+int numar_linii;
+int width, height;
 
 
 
@@ -74,7 +77,7 @@ void Permisiuni(int fis)
 
   char others_permissions_str[4];
   strcpy(others_permissions_str, mode_to_str(intrare.st_mode & S_IRWXO));
-  bytes_written = sprintf(buffer, "Drepturi de acces altii: %s\n\n", others_permissions_str);
+  bytes_written = sprintf(buffer, "Drepturi de acces altii: %s\n", others_permissions_str);
   write(fis, buffer, bytes_written);
 }
 
@@ -111,7 +114,7 @@ void Fisier_BMP(char name[], char path_name[])
       exit(-1);
     }
   
-  int width, height;
+ 
 
   lseek(fisBMP, 18, SEEK_SET); 
   read(fisBMP, &width, sizeof(int)); 
@@ -171,6 +174,55 @@ void Legatura_simbolica(char name[])
 } 
 
 
+void countLines(int fisier) {
+    lseek(fisier, 0, SEEK_SET);
+
+    char ch;
+    while (read(fisier, &ch, 1) > 0) {
+        if (ch == '\n') {
+            numar_linii++;
+        }
+    }
+   
+}
+
+
+void readBMP(char path_name[])
+{
+  int fisBMP=open(path_name, O_RDWR);
+  if(fisBMP==-1)
+    {
+      perror("eroare la deschiderea fisierului BMP");
+      exit(-1);
+    }
+  lseek(fisBMP,54,SEEK_SET);
+
+   for (int row = 0; row < height; ++row) {
+    for (int col = 0; col < width; ++col) {
+      char pixel[3];
+      read(fisBMP, pixel, sizeof(pixel));
+
+      // formula pentru obtinerea culorii gri
+      char grayscale = (char)(
+          0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0]
+      );
+
+      // inlocuim in locul pixelilor rosu albastru vedre culoarea grii
+      pixel[0] = grayscale; // Blue
+      pixel[1] = grayscale; // Green
+      pixel[2] = grayscale; // Red
+
+      // mut cursorul cu 3 pozitii inapoi pentru a putea salva modificarile
+      lseek(fisBMP, -3, SEEK_CUR);
+      write(fisBMP, pixel, sizeof(pixel));
+    }
+  }
+
+
+  close(fisBMP);
+    
+}
+
 int main(int argn,char *argv[])
 {
   if(argn!=3)
@@ -227,7 +279,17 @@ int main(int argn,char *argv[])
       
     }
   
-  //creare statistics.txt 
+  //creare statistics.txt
+
+
+   int fisier=open("statistics.txt",O_RDWR|O_CREAT, S_IRUSR|S_IWUSR|S_IXUSR|S_IROTH|S_IWOTH|S_IROTH );
+  if(fisier==-1)
+    {
+      perror("Nu s-a putut crea fisierul");
+      exit(-1);
+    }
+  else
+    printf("fisierul este deschis");
 
    char path_dir[1026];
    char new_file_name[1024];
@@ -250,63 +312,96 @@ int main(int argn,char *argv[])
     {
       if(strcmp(dirr->d_name,".")!=0 && strcmp(dirr->d_name,"..")!=0)
 	{
-
-	  sprintf(new_file_name,"%s%s",dirr->d_name,"_statistics.txt");
-	  sprintf(path_dir,"%s/%s",argv[2],new_file_name);
-	  fis=open(path_dir,O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IXUSR|S_IROTH|S_IWOTH|S_IXOTH);
-	  if(fis==-1)
+	  pid_t pid=fork();
+	  if(pid==0)
 	    {
-	      perror("nu s-a putut crea fisierul");
-	      exit(-1);
-	    }
-	  else
-	    printf("fisierul este deschis\n");
+	      numar_linii = 0;
+	      sprintf(new_file_name,"%s%s",dirr->d_name,"_statistics.txt");
+	      sprintf(path_dir,"%s/%s",argv[2],new_file_name);
+	      fis=open(path_dir,O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IXUSR|S_IROTH|S_IWOTH|S_IXOTH);
+	      if(fis==-1)
+		{
+		  perror("nu s-a putut crea fisierul");
+		  exit(-1);
+		}
+	      else
+		printf("fisierul este deschis\n");
 	  
 	 
 	  
 	  
-	  sprintf(path_name,"%s/%s",argv[1],dirr->d_name);
-	  if(lstat(path_name,&intrare)!=-1)
-	    {
+	      sprintf(path_name,"%s/%s",argv[1],dirr->d_name);
+	      if(lstat(path_name,&intrare)!=-1)
+		{
 	      
-	      if(S_ISDIR(intrare.st_mode))
-		{
-		  
-		  Director(dirr->d_name);
-		  Permisiuni(fis);
-		}
-	      
-	      if(S_ISREG(intrare.st_mode) && strstr(path_name,".bmp"))
-		{
-		  Fisier_BMP(dirr->d_name, path_name);
-		  Permisiuni(fis);
-		}
-	      else
-		{
-		  if(S_ISREG(intrare.st_mode))
+		  if(S_ISDIR(intrare.st_mode))
 		    {
-		      Fisier(dirr->d_name);
+		  
+		      Director(dirr->d_name);
 		      Permisiuni(fis);
 		    }
-		}
 	      
-	      if(S_ISLNK(intrare.st_mode))
-		{
-		  if(stat(path_name, &fisier_leg_simbolica)!=-1)
+		  if(S_ISREG(intrare.st_mode) && strstr(path_name,".bmp"))
 		    {
-		      if(S_ISREG(fisier_leg_simbolica.st_mode))
+		      Fisier_BMP(dirr->d_name, path_name);
+		      Permisiuni(fis);
+		      printf("%d %d\n",width,height);
+		      pid_t pid2=fork();
+		      if(pid2==0)
 			{
-			  Legatura_simbolica(dirr->d_name);
+			  readBMP(path_name);
+			  exit(0);
+			}
+		    }
+		  else
+		    {
+		      if(S_ISREG(intrare.st_mode))
+			{
+			  Fisier(dirr->d_name);
 			  Permisiuni(fis);
 			}
 		    }
+	      
+		  if(S_ISLNK(intrare.st_mode))
+		    {
+		      if(stat(path_name, &fisier_leg_simbolica)!=-1)
+			{
+			  if(S_ISREG(fisier_leg_simbolica.st_mode))
+			    {
+			      Legatura_simbolica(dirr->d_name);
+			      Permisiuni(fis);
+			    }
+			}
+		    }
 		}
+	      printf("aici este codul copilului\n");
+	      countLines(fis);
+	      bytes_written = sprintf(buffer, "Numar linii %s: %d\n", dirr->d_name,numar_linii);
+	      write(fisier, buffer, bytes_written);
+	      close (fis);
+	      close(fisier);
+	      exit(0);
 	    }
-	  
+	  else if (pid > 0) {
+            // părintele așteaptă un copil
+            int status;
+            waitpid(pid, &status, 0);
+
+            if (WIFEXITED(status)) {
+                bytes_written = sprintf(buffer, "S-a încheiat procesul cu pid-ul %d și codul %d\n", pid, WEXITSTATUS(status));
+                write(fisier, buffer, bytes_written);
+            }
+        } 
+        
+	
+	    
+	      
 	}
       
     }
-  close(fis);
+ 
+  close(fisier);
   closedir(dir);
+  closedir(dir2); 
   return 0;
 }
