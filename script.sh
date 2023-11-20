23809a2c3e8848bff0d8874e8db992d3112145ca
Simina Hategan <siminahategan@gmail.com>
2023-11-20 17:41:58 +0200

    adaugare script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+if test $# -ne 1
+then
+    echo "nu s-a primit exact un argument"
+    exit 1
+fi
+count=0
+while read linie
+do
+    v=`echo "$linie" | grep -e '^[A-Z][a-zA-Z0-9, ]*[\.?!]$' | grep -v " , si" | grep -v ",si" | wc -l`
+    count=`expr $count + $v` 
+done 
+
+echo $count
